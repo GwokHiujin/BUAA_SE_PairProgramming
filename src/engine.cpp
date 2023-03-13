ef688275d7181ab1c372b83c950e45866caf654c
emilyu2003 <76736029+emilyu2003@users.noreply.github.com>
2023-03-14 00:52:50 +0800

    yyy-init_useless_branch

--- src/engine.cpp
+++ src/engine.cpp
@@ -15,29 +15,7 @@ using namespace std;
 
 /* function 'engine' should return a validCode */
 
-
-vector<PII> graph[60][60];
-int topsortQueue[MAX_V], din[MAX_V], dist[MAX_E], topSortPath[MAX_E];
-int maxRingSize, maxPathSize;
-bool vis[MAX_E];
-unordered_map<char *, int> rawWordsSet;
-vector<int> path;
-vector<int> dfsVector;
-vector<string> resultVector;
-vector<PII> selfCircle[26];
-
-void init() {
-    for (int i = 0; i < 26; i++) {
-        for (int j = 0; j < 26; j++) {
-            graph[i][j].clear();
-        }
-        selfCircle[i].clear();
-    }
-    rawWordsSet.clear();
-    resultVector.clear();
-}
-
-void add(int a, int b, int c, int num) {
+void add(int a, int b, int c, int num, vector<PII> graph[60][60]) {
     graph[a][b].push_back({c, num});
 }
 
@@ -51,25 +29,7 @@ char *stringToCharPointer(string a) {
     return ans;
 }
 
-void markRawWords() {
-    for (int i = 0; i < rawWords.size(); i++) {
-        rawWordsSet[rawWords[i]] = i;
-    }
-}
-
-void buildGraph(int *options) {
-    for (auto &rawWord: rawWords) {
-        if (options[OP_J] == rawWord[0]) {
-            continue;
-        }
-        int weight = options[OP_C] ? strlen(rawWord) : 1;
-        add(rawWord[0] - 'a', rawWord[strlen(rawWord) - 1] - 'a', weight, rawWordsSet[rawWord]);
-        //printf("%c %c %d %d\n", rawWord[0], rawWord[strlen(rawWord) - 1], weight, rawWordsSet[rawWord]);
-        din[rawWord[strlen(rawWord) - 1] - 'a']++;
-    }
-}
-
-bool checkSelfCircle() {
+bool checkSelfCircle(vector<PII> graph[60][60]) {
     int cnt = 0;
     for (int i = 0; i < 26; i++) {
         if (graph[i][i].size() >= 2) {
@@ -79,7 +39,7 @@ bool checkSelfCircle() {
     return cnt < 1;
 }
 
-void deleteSelfCircle() {
+void deleteSelfCircle(vector<PII> graph[60][60], int din[MAX_V], vector<PII> selfCircle[26]) {
     for (int i = 0; i < 26; i++) {
         for (int j = 0; j < graph[i][i].size(); j++) {
             selfCircle[i].push_back(graph[i][i][j]); // the idx in rawWords
@@ -89,8 +49,8 @@ void deleteSelfCircle() {
     }
 }
 
-bool topSort() {
-    deleteSelfCircle();
+bool topSort(int topsortQueue[MAX_V], int din[MAX_V], vector<PII> selfCircle[26], vector<PII> graph[60][60]) {
+    deleteSelfCircle(graph, din, selfCircle);
     int tt = -1, hh = 0;
     for (int i = 0; i < 26; i++) {
         if (din[i] == 0) {
@@ -111,7 +71,187 @@ bool topSort() {
     return tt == 25;
 }
 
-void rebuildGraph(int *options) {
+void dfsAllLinks(int s, vector<int> path, bool vis[MAX_E], vector<string> resultVector, vector<PII> graph[60][60]) {
+    if (resultVector.size() > 20000) {
+        return;
+    }
+    if (path.size() >= 2) {
+        string str;
+        for (int i: path) {
+            str += rawWords[i];
+            str += " ";
+        }
+        resultVector.push_back(str);
+    }
+    for (int i = 0; i < 26; i++) {
+        for (int j = 0; j < graph[s][i].size(); j++) {
+            PII t = graph[s][i][j];
+            if (!vis[t.second]) {
+                vis[t.second] = true;
+                path.push_back(t.second);
+                dfsAllLinks(i, path, vis, resultVector, graph);
+                vis[t.second] = false;
+                path.pop_back();
+            }
+        }
+    }
+}
+
+void getAllLinks(vector<int> path, bool vis[MAX_E], vector<string> resultVector, vector<PII> graph[60][60]) {
+    for (int i = 0; i < 26; i++) {
+        path.clear();
+        memset(vis, 0, sizeof(vis));
+        dfsAllLinks(i, path, vis, resultVector, graph);
+    }
+}
+
+void dfsMaxRing(int s, int *options, int maxRingSize, vector<int> path, bool vis[MAX_E], vector<string> resultVector,
+                vector<PII> graph[60][60]) {  // todo: optimize
+    if (path.size() >= 2) {
+        string str = rawWords[path.back()];
+        if (!options[OP_T] || options[OP_T] == str.back()) {
+            int tmpLen = 0;
+            for (int i: path) {
+                tmpLen += (options[OP_W] ? 1 : strlen(rawWords[i]));
+            }
+            if (tmpLen > maxRingSize) {
+                maxRingSize = tmpLen;
+                resultVector.clear();
+                for (int i: path) {
+                    resultVector.push_back(rawWords[i]);
+                }
+            }
+        }
+    }
+    for (int i = 0; i < 26; i++) {
+        for (int j = 0; j < graph[s][i].size(); j++) {
+            PII t = graph[s][i][j];
+            if (!vis[t.second]) {
+                vis[t.second] = true;
+                path.push_back(t.second);
+                dfsMaxRing(i, options, maxRingSize, path, vis, resultVector, graph);
+                vis[t.second] = false;
+                path.pop_back();
+            }
+        }
+    }
+}
+
+void getMaxRing(int *options, int maxRingSize, vector<int> path, bool vis[MAX_E], vector<string> resultVector,
+                vector<PII> graph[60][60]) {
+    if (options[OP_H]) {
+        path.clear();
+        maxRingSize = 0;
+        memset(vis, 0, sizeof(vis));
+        dfsMaxRing(options[OP_H] - 'a', options, maxRingSize, path, vis, resultVector, graph);
+    } else {
+        maxRingSize = 0;
+        for (int i = 0; i < 26; i++) {
+            path.clear();
+            memset(vis, 0, sizeof(vis));
+            dfsMaxRing(i, options, maxRingSize, path, vis, resultVector, graph);
+        }
+    }
+}
+
+void dfsGetPath(int s, int *options, int maxPathSize, vector<int> path, bool vis[MAX_E], vector<string> resultVector,
+                vector<PII> graph[60][60]) {
+    if (path.size() >= 2) {
+        string str = rawWords[path.back()];
+        if (!options[OP_T] || options[OP_T] == str.back()) {
+            int tmpLen = 0;
+            for (int i: path) {
+                tmpLen += (options[OP_W] ? 1 : strlen(rawWords[i]));
+            }
+            if (tmpLen > maxPathSize) {
+                maxPathSize = tmpLen;
+                resultVector.clear();
+                for (int i: path) {
+                    resultVector.push_back(rawWords[i]);
+                }
+            }
+        }
+    }
+    for (int i = 0; i < 26; i++) {
+        for (int j = 0; j < graph[s][i].size(); j++) {
+            PII t = graph[s][i][j];
+            if (!vis[t.second]) {
+                vis[t.second] = true;
+                path.push_back(t.second);
+                dfsGetPath(i, options, maxPathSize, path, vis, resultVector, graph);
+                vis[t.second] = false;
+                path.pop_back();
+            }
+        }
+    }
+}
+
+void getPath(int *options, int maxPathSize, vector<int> path, bool vis[MAX_E], vector<string> resultVector,
+             vector<PII> graph[60][60]) {
+    if (options[OP_H]) {
+        path.clear();
+        maxPathSize = 0;
+        memset(vis, 0, sizeof(vis));
+        dfsGetPath(options[OP_H] - 'a', options, maxPathSize, path, vis, resultVector, graph);
+    } else {
+        maxPathSize = 0;
+        for (int i = 0; i < 26; i++) {
+            path.clear();
+            memset(vis, 0, sizeof(vis));
+            dfsGetPath(i, options, maxPathSize, path, vis, resultVector, graph);
+        }
+    }
+}
+
+int engine(int *options, char *res[]) { // 0-25 a-z 26-end rawWords
+    vector<PII> graph[60][60];
+    int topsortQueue[MAX_V], din[MAX_V], dist[MAX_E], topSortPath[MAX_E];
+    int maxRingSize, maxPathSize;
+    bool vis[MAX_E];
+    unordered_map<char *, int> rawWordsSet;
+    vector<int> path;
+    vector<int> dfsVector;
+    vector<string> resultVector;
+    vector<PII> selfCircle[26];
+
+    //init()
+    for (int i = 0; i < 26; i++) {
+        for (int j = 0; j < 26; j++) {
+            graph[i][j].clear();
+        }
+        selfCircle[i].clear();
+    }
+    rawWordsSet.clear();
+    resultVector.clear();
+
+    // markRawWords
+    for (int i = 0; i < rawWords.size(); i++) {
+        rawWordsSet[rawWords[i]] = i;
+    }
+
+    // buildGraph
+    for (auto &rawWord: rawWords) {
+        if (options[OP_J] == rawWord[0]) {
+            continue;
+        }
+        int weight = options[OP_C] ? strlen(rawWord) : 1;
+        add(rawWord[0] - 'a', rawWord[strlen(rawWord) - 1] - 'a', weight, rawWordsSet[rawWord], graph);
+        //printf("%c %c %d %d\n", rawWord[0], rawWord[strlen(rawWord) - 1], weight, rawWordsSet[rawWord]);
+        din[rawWord[strlen(rawWord) - 1] - 'a']++;
+    }
+
+    if (!options[OP_R]) {
+        if (!checkSelfCircle(graph)) {
+            throw bugReport(BUG_RING_EXIST);
+        }
+        if (!topSort(topsortQueue, din, selfCircle, graph)) {
+            throw bugReport(BUG_RING_EXIST);
+        }
+    }
+
+    memset(din, 0, sizeof(din));
+
+    // rebuildGraph
     for (int i = 0; i < 26; i++) {
         for (int j = 0; j < selfCircle[i].size(); j++) {
             graph[i][i].push_back(selfCircle[i][j]);
@@ -145,160 +285,14 @@ void rebuildGraph(int *options) {
             }
         }
     }
-}
 
-void dfsAllLinks(int s) {
-    if (resultVector.size() > 20000) {
-        return;
-    }
-    if (path.size() >= 2) {
-        string str;
-        for (int i: path) {
-            str += rawWords[i];
-            str += " ";
-        }
-        resultVector.push_back(str);
-    }
-    for (int i = 0; i < 26; i++) {
-        for (int j = 0; j < graph[s][i].size(); j++) {
-            PII t = graph[s][i][j];
-            if (!vis[t.second]) {
-                vis[t.second] = true;
-                path.push_back(t.second);
-                dfsAllLinks(i);
-                vis[t.second] = false;
-                path.pop_back();
-            }
-        }
-    }
-}
-
-void getAllLinks() {
-    for (int i = 0; i < 26; i++) {
-        path.clear();
-        memset(vis, 0, sizeof(vis));
-        dfsAllLinks(i);
-    }
-}
-
-void dfsMaxRing(int s, int *options) {  // todo: optimize
-    if (path.size() >= 2) {
-        string str = rawWords[path.back()];
-        if (!options[OP_T] || options[OP_T] == str.back()) {
-            int tmpLen = 0;
-            for (int i: path) {
-                tmpLen += (options[OP_W] ? 1 : strlen(rawWords[i]));
-            }
-            if (tmpLen > maxRingSize) {
-                maxRingSize = tmpLen;
-                resultVector.clear();
-                for (int i: path) {
-                    resultVector.push_back(rawWords[i]);
-                }
-            }
-        }
-    }
-    for (int i = 0; i < 26; i++) {
-        for (int j = 0; j < graph[s][i].size(); j++) {
-            PII t = graph[s][i][j];
-            if (!vis[t.second]) {
-                vis[t.second] = true;
-                path.push_back(t.second);
-                dfsMaxRing(i, options);
-                vis[t.second] = false;
-                path.pop_back();
-            }
-        }
-    }
-}
-
-void getMaxRing(int *options) {
-    if (options[OP_H]) {
-        path.clear();
-        maxRingSize = 0;
-        memset(vis, 0, sizeof(vis));
-        dfsMaxRing(options[OP_H] - 'a', options);
-    } else {
-        maxRingSize = 0;
-        for (int i = 0; i < 26; i++) {
-            path.clear();
-            memset(vis, 0, sizeof(vis));
-            dfsMaxRing(i, options);
-        }
-    }
-}
-
-void dfsGetPath(int s, int *options) {
-    if (path.size() >= 2) {
-        string str = rawWords[path.back()];
-        if (!options[OP_T] || options[OP_T] == str.back()) {
-            int tmpLen = 0;
-            for (int i: path) {
-                tmpLen += (options[OP_W] ? 1 : strlen(rawWords[i]));
-            }
-            if (tmpLen > maxPathSize) {
-                maxPathSize = tmpLen;
-                resultVector.clear();
-                for (int i: path) {
-                    resultVector.push_back(rawWords[i]);
-                }
-            }
-        }
-    }
-    for (int i = 0; i < 26; i++) {
-        for (int j = 0; j < graph[s][i].size(); j++) {
-            PII t = graph[s][i][j];
-            if (!vis[t.second]) {
-                vis[t.second] = true;
-                path.push_back(t.second);
-                dfsGetPath(i, options);
-                vis[t.second] = false;
-                path.pop_back();
-            }
-        }
-    }
-}
-
-void getPath(int *options) {
-    if (options[OP_H]) {
-        path.clear();
-        maxPathSize = 0;
-        memset(vis, 0, sizeof(vis));
-        dfsGetPath(options[OP_H] - 'a', options);
-    } else {
-        maxPathSize = 0;
-        for (int i = 0; i < 26; i++) {
-            path.clear();
-            memset(vis, 0, sizeof(vis));
-            dfsGetPath(i, options);
-        }
-    }
-}
-
-int engine(int *options, char *res[]) { // 0-25 a-z 26-end rawWords
-    init();
-    markRawWords();
-
-    buildGraph(options);
-
-    if (!options[OP_R]) {
-        if (!checkSelfCircle()) {
-            throw bugReport(BUG_RING_EXIST);
-        }
-        if (!topSort()) {
-            throw bugReport(BUG_RING_EXIST);
-        }
-    }
-
-    memset(din, 0, sizeof(din));
-    rebuildGraph(options);
 
     if (options[OP_N]) {
-        getAllLinks();
+        getAllLinks(path, vis, resultVector, graph);
     } else if (options[OP_R]) {
-        getMaxRing(options);
+        getMaxRing(options, maxRingSize, path, vis, resultVector, graph);
     } else {
-        getPath(options);
+        getPath(options, maxPathSize, path, vis, resultVector, graph);
 //        if (options[OP_T]) {
 //            memset(dist, -0x3f, sizeof(dist));
 //            dist[options[OP_T]] = 0;
