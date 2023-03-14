874ecf7669c1f582111fc63c057336de1d997816
emilyu2003 <76736029+emilyu2003@users.noreply.github.com>
2023-03-14 16:39:19 +0800

    yyy-add_gitignore

--- src/include/output.h
+++
@@ -1,9 +0,0 @@
-#ifndef CMAKETEST_OUTPUT_H
-#define CMAKETEST_OUTPUT_H
-
-#include <vector>
-#include <string>
-
-void output(const int *options, char* result[], int ans);
-
-#endif //CMAKETEST_OUTPUT_H
